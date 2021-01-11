2513e49fe38d61e501e688043e7e417dd83791ee
10EastSea <xgf6580@gmail.com>
2021-01-12 01:49:18 +0900

    스코어 리스트 업로드

---
+++ app/src/main/java/com/example/cs496_proj2/CSCal/ScoreListActivity.java
@@ -0,0 +1,16 @@
+package com.example.cs496_proj2.CSCal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cs496_proj2.R;
+
+public class ScoreListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_list);
+    }
+}
