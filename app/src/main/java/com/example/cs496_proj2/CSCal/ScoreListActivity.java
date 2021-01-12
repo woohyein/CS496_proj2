7725e8e88d1e730fefda82fc6235f0c998866aa8
10EastSea <xgf6580@gmail.com>
2021-01-12 13:42:39 +0900

    스코어 리스트 1차 수정

--- app/src/main/java/com/example/cs496_proj2/CSCal/ScoreListActivity.java
+++ app/src/main/java/com/example/cs496_proj2/CSCal/ScoreListActivity.java
@@ -1,16 +1,202 @@
 package com.example.cs496_proj2.CSCal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.cs496_proj2.GlobalId;
 import com.example.cs496_proj2.R;
+import com.example.cs496_proj2.contacts.Contact;
+import com.example.cs496_proj2.contacts.GlobalContacts;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class ScoreListActivity extends AppCompatActivity {
+
+    ArrayList<String> playerList = new ArrayList<String>();
+    ArrayList<String> scoreList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_list);
+
+
+    }
+
+    public void getPlayerScore() {
+
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            if(urls[0].equals("http://192.249.18.228:3000/receive")) {
+                try {
+                    HttpURLConnection con = null;
+                    BufferedReader reader = null;
+
+                    try{
+                        //URL url = new URL("http://192.168.25.16:3000/users");
+                        URL url = new URL(urls[0]);
+                        //연결을 함
+                        con = (HttpURLConnection) url.openConnection();
+
+                        con.setRequestMethod("POST");//POST방식으로 보냄
+                        con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                        con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+                        con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                        con.setDoOutput(false);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                        con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                        con.connect();
+
+                        //서버로 부터 데이터를 받음
+                        InputStream stream = con.getInputStream();
+
+                        reader = new BufferedReader(new InputStreamReader(stream));
+
+                        StringBuffer buffer = new StringBuffer();
+
+                        String line = "";
+                        while((line = reader.readLine()) != null){
+                            buffer.append(line);
+                        }
+
+                        // Log.d("asdf", buffer.toString());
+                        jsonParsing(buffer.toString());
+
+                        return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                    } catch (MalformedURLException e){
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if(con != null){
+                            con.disconnect();
+                        }
+                        try {
+                            if(reader != null){
+                                reader.close();//버퍼를 닫아줌
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if(urls[0].equals("http://192.249.18.228:3000/take")){
+                try {
+                    //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.accumulate("user", GlobalId.getInstance().getId());
+
+                    HttpURLConnection con = null;
+                    BufferedReader reader = null;
+
+                    try {
+                        //URL url = new URL("http://192.168.25.16:3000/users");
+                        URL url = new URL(urls[0]);
+                        //연결을 함
+                        con = (HttpURLConnection) url.openConnection();
+
+                        con.setRequestMethod("POST");//POST방식으로 보냄
+                        con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                        con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+
+
+                        con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                        con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                        con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                        con.connect();
+
+                        //서버로 보내기위해서 스트림 만듬
+                        OutputStream outStream = con.getOutputStream();
+                        //버퍼를 생성하고 넣음
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                        writer.write(jsonObject.toString());
+                        writer.flush();
+                        writer.close();//버퍼를 받아줌
+
+                        //서버로 부터 데이터를 받음
+                        InputStream stream = con.getInputStream();
+
+                        reader = new BufferedReader(new InputStreamReader(stream));
+
+                        StringBuffer buffer = new StringBuffer();
+
+                        String line = "";
+                        while((line = reader.readLine()) != null){
+                            buffer.append(line);
+                        }
+
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (con != null) {
+                            con.disconnect();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+    }
+
+    private void jsonParsing(String json)
+    {
+        try{
+            ArrayList<Contact> newContact = new ArrayList<Contact>();
+
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray contactArray = jsonObject.getJSONArray("player_score");
+            for(int i=0; i<contactArray.length(); i++) {
+                JSONObject contactObject = contactArray.getJSONObject(i);
+
+                Contact contact;
+                String image = contactObject.getString("image");
+                if(image.equals("null")) contact = new Contact(contactObject.getString("phone"), contactObject.getString("fullName"), null);
+                else contact = new Contact(contactObject.getString("phone"), contactObject.getString("fullName"), image);
+
+                Log.d("asdf", contact.getMsg());
+
+                newContact.add(contact);
+            }
+
+            GlobalContacts.getInstance().setContacts(newContact);
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
